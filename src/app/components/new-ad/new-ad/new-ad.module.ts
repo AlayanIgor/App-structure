d71e5b8eb1c8d997f7f22c59f441f8eb03acf19e
Igor Alayan <alayan_igor@mail.ru>
2023-09-10 23:26:16 +0300

    added even more components

---
+++ src/app/components/new-ad/new-ad/new-ad.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewAdComponent } from './new-ad/new-ad.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewAdComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NewAdModule { }

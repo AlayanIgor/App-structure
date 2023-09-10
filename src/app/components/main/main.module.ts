978a407a1ab853958e9a22252e3491a72d05acff
Igor Alayan <alayan_igor@mail.ru>
2023-09-10 23:15:16 +0300

    added more components

--- src/app/components/main/main.module.ts
+++ src/app/components/main/main.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { CatalogComponent } from './components/catalog/catalog.component';
 import { CatalogWithSidebarComponent } from './components/catalog-with-sidebar/catalog-with-sidebar.component';
-
+import { ProductCardComponent } from './components/catalog/product-card/product-card.component';
+import { SidebarComponent } from './components/catalog-with-sidebar/sidebar/sidebar.component';
+import { CategoriesComponent } from './components/catalog/categories/categories.component';
 
 @NgModule({
   declarations: [
     CatalogComponent,
-    CatalogWithSidebarComponent
+    CatalogWithSidebarComponent,
+    ProductCardComponent,
+    SidebarComponent,
+    CategoriesComponent,
   ],
-  imports: [
-    CommonModule,
-    MainRoutingModule
-  ]
+  imports: [CommonModule, MainRoutingModule],
 })
-export class MainModule { }
+export class MainModule {}
